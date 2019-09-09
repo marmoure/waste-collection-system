138fdd78696faf5e924c0cc5e29bf1799a10909a
marmoure <younesmirou@outlook.com>
2019-09-09 21:13:10 +0000

    adds on

--- index.js
+++ index.js
@@ -11,56 +11,10 @@ const server = http.createServer(app);
 const socketIo = require('socket.io');
 const io = socketIo(server);
 const usersData = [];
-
 let trigger = () => {};
-fs.readFile('data.json', 'utf8', function(err, contents) {
-    let str = JSON.parse(contents);
-     str.users.forEach((e) => {
-      usersData.push(e);
-     });
-     console.log(usersData);
-});
 
-app.use(express.static(path.join(__dirname,"public")));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.get("");
 
-app.post("/haja",(req,res) => {
-    const {name,password} = req.body;
-    let type = checkCredentials(name,password);
-    if( type != 0) {
-        console.log(type);
-        if(type == 3)
-            res.redirect(200,'/view.html');
-        if(type == 2)
-            res.redirect(200,'/tech.html');
-        if(type == 1)
-            res.redirect(200,'/view.html');
-    }else {
-        res.redirect(401, '/login.html');
-    }
-});
-server.listen(3000, err => {
-
- if(err){
-        throw err;
-    }
-    console.log("server running on port 3000");
-});
-
-const checkCredentials = (name,pass) => {
-    let valid = 0;
-    usersData.forEach((e) => {
-        if(e.name === name) {
-            if(e.password === pass) {
-                valid = e.type;
-            };
-        };
-    });
-    return valid;
-};
-
+//mqtt
 const mqtt = require('mqtt');
 const options = {
     protocolId: 'MQTT', // Or 'MQIsdp' in MQTT 3.1 and 5.0
@@ -73,6 +27,7 @@ const options = {
 const client  = mqtt.connect('tcp://m16.cloudmqtt.com:15142',options);
 
 
+//mqttfunction
 client.on('connect',  () => {
     client.subscribe('here',(err) => {
       if (!err) {
@@ -111,11 +66,63 @@ client.on('message', (topic, message) => {
         trigger();
     }
 });
+//mqttfunction
 
-setInterval(() => {
-    // client.publish('alive', "who's there");
-},7000);
+//mqtt
+// database
 
+const mysql = require('mysql');
+const con = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "",
+      database:"work"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("DATABASE Connected!");
+});
+
+
+
+//   con.query(`INSERT INTO users (id, username, pass, usesrtype, reg_date) VALUES (NULL, '${name}', '${pass}', '${userType}', CURRENT_TIMESTAMP)`, function (error, results, fields) {
+//     if (error) throw error;
+//     console.log(results);
+//   });
+//   con.query(`SELECT * FROM users`, function (error, results, fields) {
+//     if (error) throw error;
+//     console.log(results);
+//   });
+//   con.end();
+
+// database
+
+//express server
+app.use(express.static(path.join(__dirname,"public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/haja",(req,res) => {
+    const {name,password} = req.body;
+   checkCredentials(name,password,res);
+});
+
+app.post("/add",(req,res) => {
+    console.log(req.body);
+    //TODO add data to database
+});
+
+server.listen(3000, err => {
+ if(err){
+        throw err;
+    }
+    console.log("server running on port 3000");
+});
+//experss server
+
+
+//socketio
 io.on("connection", socket => {
     socket.on("_ping", () => {
         socket.emit("_pong");
@@ -141,6 +148,42 @@ io.on("connection", socket => {
         socket.emit("adminUpdate",transitString);
     };
 });
+//socketio
 
+
+
+// my functions
+const checkCredentials = (name,pass,res) => {
+    //TODO use database
+    let mytype = 0;
+    con.query(`SELECT * FROM users WHERE username = "${name}"`, function (error, results, fields) {
+        if (error) {
+            mytype = 0;
+            console.log("EROOOR in query");
+        }else {
+            let mypass = results[0].pass;
+            console.log(mypass);
+            if (mypass == pass) {
+                mytype=results[0].usesrtype ;
+                redirectUser(mytype,res);
+            }
+        }
+    });
+};
+
+
+const redirectUser = (type,res) => {
+    if( type != 0) {
+        console.log(type);
+        if(type == 3)
+            res.redirect(200,'/view.html');
+        if(type == 2)
+            res.redirect(200,'/tech.html');
+        if(type == 1)
+            res.redirect(200,'/admin.html');
+    }else {
+        res.redirect(401, '/');
+    }
+}
 
 // {"id":"arduino","data":{"temperature":20.80,"humidity":63.00,"full":15.00,"lat":36.084032,"long":7.26024}}
